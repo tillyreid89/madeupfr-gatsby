41eb183e97b6a96fde26a0eb82654b59f1207ac1
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2017-12-31 15:44:40 +0100

    Fonts

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import Link from "gatsby-link";
 import Helmet from "react-helmet";
 import logoBlack from "../img/logoBlack.svg";
+import logoWhite from "../img/logoWhite.svg";
 import "./all.sass";
 
 const Navbar = () => (
@@ -30,6 +31,88 @@ const Navbar = () => (
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet title="Home | Gatsby + Netlify CMS" />
+
+    <section className="hero is-primary is-medium">
+      {/* <div className="hero-head">
+        <nav className="navbar">
+          <div className="container">
+            <div className="navbar-brand">
+              <a className="navbar-item">
+                <img
+                  src="https://bulma.io/images/bulma-type-white.png"
+                  alt="Logo"
+                />
+              </a>
+              <span className="navbar-burger burger" data-target="navbarMenuHeroA">
+                <span />
+                <span />
+                <span />
+              </span>
+            </div>
+            <div id="navbarMenuHeroA" className="navbar-menu">
+              <div className="navbar-end">
+                <a className="navbar-item is-active">Home</a>
+                <a className="navbar-item">Examples</a>
+                <a className="navbar-item">Documentation</a>
+                <span className="navbar-item">
+                  <a className="button is-primary is-inverted">
+                    <span className="icon">
+                      <i className="fa fa-github" />
+                    </span>
+                    <span>Download</span>
+                  </a>
+                </span>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div> */}
+
+      <div
+        className="hero-body"
+        style={{
+          backgroundRepeat: "no-repeat",
+          backgroundAttachment: "fixed",
+          backgroundPosition: "center bottom",
+          backgroundImage: "url(/img/saintropez.jpg)",
+          backgroundSize: "cover"
+        }}
+      >
+        <div className="container has-text-centered">
+          <img src={logoWhite} alt="Kaldi" style={{ width: "30%" }} />
+          {/* <h1 className="title">Title</h1> */}
+          <h2 className="subtitle">Subtitle</h2>
+        </div>
+      </div>
+      {/*
+      <div className="hero-foot">
+        <nav className="tabs">
+          <div className="container">
+            <ul>
+              <li className="is-active">
+                <a>Overview</a>
+              </li>
+              <li>
+                <a>Modifiers</a>
+              </li>
+              <li>
+                <a>Grid</a>
+              </li>
+              <li>
+                <a>Elements</a>
+              </li>
+              <li>
+                <a>Components</a>
+              </li>
+              <li>
+                <a>Layout</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div> */}
+    </section>
+
     <Navbar />
     <div>{children()}</div>
   </div>
